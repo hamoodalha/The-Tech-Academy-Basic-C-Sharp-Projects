7158e8a784bfa008c59bfb5a368bd425664375e4
hamoodalha <ibtissam929@gmail.com>
2026-05-04 16:26:27 -0400

    Add files via upload

---
+++ IQuittable.cs
@@ -0,0 +1,13 @@
+// This is an interface called IQuittable
+
+// It just tells classes they must have a Quit method
+
+interface IQuittable
+
+{
+
+    // This method does not return anything
+
+    void Quit();
+
+}
